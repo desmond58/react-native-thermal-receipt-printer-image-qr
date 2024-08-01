b14c745ea7198287a606f95d0db424adaa41edf1
desmond58 <chiping5858@gmail.com>
2024-08-01 15:34:17 +0800

    testing

--- android/src/main/java/com/pinmi/react/printer/adapter/USBPrinterAdapter.java
+++ android/src/main/java/com/pinmi/react/printer/adapter/USBPrinterAdapter.java
@@ -106,8 +106,14 @@ public class USBPrinterAdapter implements PrinterAdapter {
         this.mContext = reactContext;
         this.mUSBManager = (UsbManager) this.mContext.getSystemService(Context.USB_SERVICE);
 
-        int flag = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S ? PendingIntent.FLAG_MUTABLE : 0;
-        this.mPermissionIndent = PendingIntent.getBroadcast(mContext, 0, new Intent(ACTION_USB_PERMISSION), flag | PendingIntent.FLAG_UPDATE_CURRENT);
+        int flag;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            flag = PendingIntent.FLAG_IMMUTABLE;
+        } else {
+            flag = PendingIntent.FLAG_UPDATE_CURRENT;
+        }
+
+        this.mPermissionIndent = PendingIntent.getBroadcast(mContext, 0, new Intent(ACTION_USB_PERMISSION), flag);
 
         IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
         filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
